8056c66b60f121fd176d7999fe88fb0f125398a7
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-05-22 17:40:25 +0400

    Refactor main function to run tests

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "kaizen.h"
 
 bool isPowerOfTwo(unsigned int x) {
@@ -17,48 +18,87 @@ void swapWithTemp(int& a, int& b) {
 }
 
 void swapWithXOR(int& a, int& b) {
-	if (&a == &b)
+	if (a == b)
 		return;
 	a ^= b;
 	b ^= a;
 	a ^= b;
 }
 
-int main() {
-	
-	for (int x = 0; x < 50; x++)
-	{
-		if (isPowerOfTwo(x))
-			std::cout << x << " is a power of two." << std::endl;
-		// else
-		// 	std::cout << x << " is not a power of two." << std::endl;
-	}
+void testIsPowerOfTwo() {
+	assert(!isPowerOfTwo(0));
+	assert(isPowerOfTwo(1));
+	assert(isPowerOfTwo(2));
+	assert(!isPowerOfTwo(3));
+	assert(isPowerOfTwo(4));
+	assert(!isPowerOfTwo(6));
+	assert(isPowerOfTwo(8));
+	assert(!isPowerOfTwo(9));
+	assert(isPowerOfTwo(1024));
+	std::cout << "[OK] isPowerOfTwo passed all tests.\n\n";
+}
 
-	if (isLittleEndian())
-		std::cout << "Little Endian\n";
-	else
-		std::cout << "Big Endian\n";
+void testIsLittleEndian() {
+	bool result = isLittleEndian();
+	std::cout << "[Info] Machine is " << (result ? "Little Endian" : "Big Endian") << "\n\n";
+}
 
+void testSwapWithTemp() {
+	int a = 5, b = 10;
+	swapWithTemp(a, b);
+	assert(a == 10 && b == 5);
+
+	a = -7; b = 42;
+	swapWithTemp(a, b);
+	assert(a == 42 && b == -7);
+
+	std::cout << "[OK] swapWithTemp passed all tests.\n\n";
+}
+
+void testSwapWithXOR() {
+	int a = 7, b = 13;
+	swapWithXOR(a, b);
+	assert(a == 13 && b == 7);
+
+	a = 100;
+	swapWithXOR(a, a);
+	assert(a == 100);
+
+	std::cout << "[OK] swapWithXOR passed all tests.\n\n";
+}
+
+void testSwapTime() {
 	constexpr size_t N = 100000000;
-	int a = 123;
-	int b = 456;
+	int a = 123, b = 456;
 
 	auto timer = zen::timer();
 	timer.start();
 	for (size_t i = 0; i < N; ++i)
 		swapWithTemp(a, b);
 	timer.stop();
-	auto tempDuration = timer.duration<zen::timer::msec>();
+	auto tempDuration = timer.duration<zen::timer::nsec>();
 
 	timer.start();
 	for (size_t i = 0; i < N; ++i)
 		swapWithXOR(a, b);
 	timer.stop();
-	auto xorDuration = timer.duration<zen::timer::msec>();
+	auto xorDuration = timer.duration<zen::timer::nsec>();
 
-	std::cout << "Swap with temp took: " << tempDuration.count() << " ms\n";
-	std::cout << "Swap with XOR  took: " << xorDuration.count() << " ms\n";
+	std::cout << "[Info] Swap with temp took: " << tempDuration.count() << " ns\n";
+	std::cout << "[Info] Swap with XOR  took: " << xorDuration.count() << " ns\n";
+	std::cout << "[Info] Swap with temp is " << (tempDuration < xorDuration ? "faster" : "slower") << " than swap with XOR.\n";
+}
 
+void runAllTests() {
+	testIsPowerOfTwo();
+	testIsLittleEndian();
+	testSwapWithTemp();
+	testSwapWithXOR();
+	testSwapTime();
+}
+
+int main() {
+	runAllTests();
 	return 0;
 }
 
